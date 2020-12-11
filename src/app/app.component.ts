4c33cfc506c09793920d8510c1fc968781dbca43
PERRINN <perrinnlimited@gmail.com>
2020-12-11 09:35:44 +0000

    UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,9 +24,9 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
         <div style="float:left;width:100px;text-align:center;height:33px;cursor:pointer;line-height:31px;font-size:11px;color:blue;border-style:solid;border-width:0 1px 0 0;border-color:#ddd" (click)="newMessage()">New message</div>
         <img class='editButton' style="float:right;width:20px;opacity:.6" (click)="router.navigate(['settings'])" src="./../assets/App icons/settings-24px.svg">
         <div style="float:right;height:33px;border-style:solid;border-width:0 1px 0 0;border-color:#ddd"></div>
-        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:20px;filter:grayscale(100%)" src="./../assets/App icons/onshapeLogo.png" onclick="window.open('https://cad.onshape.com/documents?nodeId=31475a51a48fbcc9cfc7e244&resourceType=folder','_blank')">
-        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:20px;filter:grayscale(100%)" src="./../assets/App icons/Google_Drive_icon_(2020).svg" onclick="window.open('https://drive.google.com/drive/u/1/folders/1qvipN1gs1QS4sCh1tY8rSSFXV5S0-uR3','_blank')">
-        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:20px;margin-top:6px;filter:grayscale(100%)" src="./../assets/App icons/google-meet-logo.png" onclick="window.open('https://meet.google.com/rxn-vtfa-shq','_blank')">
+        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:20px;filter:grayscale(100%)" src="./../assets/App icons/onshape_new.png" onclick="window.open('https://cad.onshape.com/documents?nodeId=31475a51a48fbcc9cfc7e244&resourceType=folder','_blank')">
+        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:18px;margin-top:7px;filter:grayscale(100%)" src="./../assets/App icons/Google_Drive_icon_(2020).svg" onclick="window.open('https://drive.google.com/drive/u/1/folders/1qvipN1gs1QS4sCh1tY8rSSFXV5S0-uR3','_blank')">
+        <img *ngIf="UI.currentUserLastMessageObj?.PERRINN?.wallet?.balance>0" class='editButton' style="float:right;width:18px;margin-top:8px;filter:grayscale(100%)" src="./../assets/App icons/google-meet-logo.png" onclick="window.open('https://meet.google.com/rxn-vtfa-shq','_blank')">
       </div>
       <div class="seperator" style="width:100%;margin:0px"></div>
     </div>
