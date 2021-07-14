5bb02de984ae9ca07bc8a31e9b9180399709d531
PERRINN <perrinnlimited@gmail.com>
2021-07-14 17:29:26 +0100

    Update chat.component.ts

--- src/app/chat.component.ts
+++ src/app/chat.component.ts
@@ -175,7 +175,7 @@ import * as firebase from 'firebase/app'
             <div class='messageFooter' style="cursor:pointer;clear:both;height:15px" (click)="messageShowActions.includes(message.key)?messageShowActions.splice(messageShowActions.indexOf(message.key),1):messageShowActions.push(message.key)">
               <div style="float:left;width:100px;text-align:right;line-height:10px">...</div>
               <span *ngIf="message.payload?.verified" class="material-icons" style="float:right;font-size:15px;margin:0 2px 2px 0;color:green">done</span>
-              <span *ngIf="message.payload?.contract?.amount>0" style="float:right;font-size:10px;margin:0 5px 2px 0;line-height:15px;color:green">+{{UI.formatCOINS(message.payload?.contract?.amount)}}</span>
+              <span *ngIf="message.payload?.contract?.amount>0" style="float:right;font-size:10px;margin:0 5px 2px 0;line-height:15px;color:green">+{{UI.formatCOINS(message.payload?.contract?.amount)}} ({{secondsToDhmDetail1(message.payload?.contract?.days*3600*24)}})</span>
               <span *ngIf="message.payload?.userChain?.nextMessage=='none'&&message.payload?.wallet?.balance!=undefined" style="float:right;font-size:10px;margin:0 5px 2px 0;line-height:15px;color:#999">{{UI.formatCOINS(message.payload?.wallet?.balance)}}</span>
             </div>
             <div *ngIf="messageShowActions.includes(message.key)">
